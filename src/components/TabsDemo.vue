b9cc77e86dae5ae342b304f316cb70fc9a88c675
Kasan <879611700@qq.com>
2023-02-17 13:45:26 +0800

    更正TabsDemo的显示错误

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -3,9 +3,9 @@
         <Tabs v-model:selected="activeName">
             <Tab title="导航1">Tab1 <Button type="primary">按钮1</Button></Tab>
             <Tab title="导航2">Tab2 <Button type="primary">按钮2</Button></Tab>
-            <Tab title="导航3">Tab2 <Button type="primary">按钮2</Button></Tab>
-            <Tab title="导航4">Tab2 <Button type="primary">按钮2</Button></Tab>
-            <Tab title="导航5">Tab2 <Button type="primary">按钮2</Button></Tab>
+            <Tab title="导航3">Tab3 <Button type="primary">按钮3</Button></Tab>
+            <Tab title="导航4">Tab4 <Button type="primary">按钮4</Button></Tab>
+            <Tab title="导航5">Tab5 <Button type="primary">按钮5</Button></Tab>
         </Tabs>
         
     </div>
